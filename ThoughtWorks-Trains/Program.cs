48324e6ac5bd5d14ac0f5b81e4c793dba71c1434
Ishan Chatterjee <ishan.chatterjee@gmail.com>
2011-12-01 12:29:13 -0500

    Generated test

--- ThoughtWorks-Trains/Program.cs
+++ ThoughtWorks-Trains/Program.cs
@@ -7,5 +7,22 @@ namespace ThoughtWorks_Trains {
 	class Program {
 		static void Main(string[] args) {
 		}
+
+		/// <summary>
+		/// Adds a new endpoint
+		/// </summary>
+		public void AddVertex() {
+
+		}
+
+		/// <summary>
+		/// Adds a directed route
+		/// </summary>
+		public void AddEdge() {
+		}
+
+		public bool IsReachable() {
+			return false;
+		}
 	}
 }
